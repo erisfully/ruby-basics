0ee2247e0dd777b4cedef2a3945fe05bdcba1b8a
Ashley <a.clarkem@gmail.com>
2014-06-16 15:50:49 -0600

    grandmother

---
+++ grandmother.rb
@@ -0,0 +1,15 @@
+puts "Have been to the movies?"
+answer = gets.chomp
+
+while answer == answer.downcase
+  puts "HUH!? SPEAK UP SONNY!"
+  answer = gets.chomp
+
+  if answer == answer.upcase
+  puts "NO, NOT SINCE " + (rand(1951-1930) + 1930).to_s
+  end
+end
+
+
+
+
