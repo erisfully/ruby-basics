e6fc83b8e7b05cba2cc2768b03b63a4e221a4ee6
Ashley <a.clarkem@gmail.com>
2014-06-17 23:44:58 -0600

    finished beers

--- 99_beers.rb
+++ 99_beers.rb
@@ -1,14 +1,28 @@
+
 puts "Let's sing! How many beers are there on the wall?"
 
 reply = gets.chomp
 
-while reply == ''
+
+if reply == ''
   puts "I need to know how many bottles to sing!"
   reply = gets.chomp
+end
 
 full_reply = reply.split
-  if full_reply[0] == 'sing' && full_reply[1] == '8'
-    puts "8 bottles on the wall"
-  else puts "ERROR"
+
+
+if full_reply[0] != "sing" || full_reply[1].to_i <= 0
+puts "ERROR!!"
+
+else full_reply[0] == "sing" && full_reply[1].to_i > 0
+  puts full_reply[1] + " bottles of beer on the wall"
+  puts full_reply[1] + " bottles of beer"
+  puts "take one down and pass it around"
+  puts (full_reply[1].to_i - 1).to_s + " bottles of beer of the wall!"
+
 end
-end
+
+
+
+
